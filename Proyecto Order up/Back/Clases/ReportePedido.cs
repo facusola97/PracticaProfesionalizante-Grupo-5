e30c4c2b8de9285e479397eec2a56c495573070a
facusola97 <114526665+facusola97@users.noreply.github.com>
2023-09-25 19:26:42 -0300

    back

---
+++ Proyecto Order up/Back/Clases/ReportePedido.cs
@@ -0,0 +1,24 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Back.Clases
+{
+    public class ReportePedidos
+    {
+        [Key]
+        public int IdReporte { get; set; }
+
+        public Reporte reporte { get; set; }        
+
+        public int IdPedido { get; set; }
+
+        public Pedido pedido { get; set; }
+
+       
+    }
+}
---
+++ Proyecto Order up/Back/ClasesData/PedidoData.cs
@@ -0,0 +1,47 @@
+﻿using Back.Clases;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Back.ClasesData
+{
+    internal class PedidoData
+    {
+        private readonly ApplicationDbContext _context;
+
+        public PedidoData(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public void AgregarPedido(Pedido pedidoNuevo)
+        {
+            _context.pedidos.Add(pedidoNuevo);
+            _context.SaveChanges();
+        }
+        public void EliminarPedido(int IdPedido)
+        {
+            var PedidoEncontrado = _context.pedidos.Find(IdPedido);
+            if (PedidoEncontrado != null)
+            {
+                _context.pedidos.Remove(PedidoEncontrado);
+                _context.SaveChanges();
+            }
+        }
+
+        //public void ModificarPedido(Pedido PedidoModificado)
+        //{
+        //    var PedidoEncontrado = _context.pedidos.Find(PedidoModificado.IdPedido);
+        //    if (PedidoEncontrado != null)
+        //    {
+        //        PedidoEncontrado.Precio = PedidoModificado.IdPedido;
+        //        _context.SaveChanges();
+        //    }
+        }
+
+
+
+    }
+}
